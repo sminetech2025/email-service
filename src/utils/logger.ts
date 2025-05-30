46dc70f658fadcc66af09a4824c13c42bdde2669
Shwetansh Srivastava <shwetansh.srivastava@cloudoniqtechnologies.com>
2025-05-30 11:54:50 +0000

    added render.yaml file

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -12,9 +12,7 @@ const logger = winston.createLogger({
         winston.format.colorize(),
         winston.format.simple()
       )
-    }),
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' })
+    })
   ]
 });
 
